3e06ed7718034afe2e9cc681106db5f6085281af
ryohei_1540 <ryohei_1540@hotmail.co.jp>
2024-06-08 15:47:48 +0000

    hoge

--- sql/base.sql
+++ sql/base.sql
@@ -1,3 +1,5 @@
 EXECUTE IMMEDIATE FROM './tables/simple_table.sql';
 EXECUTE IMMEDIATE FROM './procedures/simple_procedure.sql';
 EXECUTE IMMEDIATE FROM './tasks/simple_task.sql';
+EXECUTE IMMEDIATE FROM './procedures/hello.sql';
+EXECUTE IMMEDIATE FROM './tasks/hello_task.sql';
