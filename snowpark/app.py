eb2338f12790b9b0e62482a6cee4562cbb2498fd
ryohei_1540 <ryohei_1540@hotmail.co.jp>
2024-06-08 16:23:21 +0000

    hoge

--- snowpark/app.py
+++ snowpark/app.py
@@ -2,6 +2,7 @@ from snowflake.snowpark import Session
 
 def return_simple_table(session):
     # 既存のテーブルを読み込み
-    session.sql("INSERT INTO simple_table (id, value, created_at) SELECT UNIFORM(1, 100, RANDOM()), 'hoge', CURRENT_TIMESTAMP()").collect()
+    # session.sql("INSERT INTO simple_table (id, value, created_at) SELECT UNIFORM(1, 100, RANDOM()), 'hoge', CURRENT_TIMESTAMP()").collect()
+    session.sql("INSERT INTO simple_table (id, value, created_at) SELECT UNIFORM(1, 100, RANDOM()), 'hoge2', CURRENT_TIMESTAMP()").collect()
     return 'ok'
 
