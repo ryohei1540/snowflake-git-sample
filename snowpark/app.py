b10574ebe5876963188073601babb498fd59d8dc
ryohei_1540 <ryohei_1540@hotmail.co.jp>
2024-06-08 15:51:45 +0000

    hoge

--- snowpark/app.py
+++ snowpark/app.py
@@ -7,7 +7,8 @@ def return_simple_table(session):
     df = session.table("simple_table")
     
     # 新しい行をDataFrameとして作成
-    new_row = session.create_dataframe([[1, 'New Name', datetime.now()]], schema=["id", "name", "created_at"])
+    # new_row = session.create_dataframe([[1, 'New Name', datetime.now()]], schema=["id", "name", "created_at"])
+    new_row = session.create_dataframe([[999, 'New Name', datetime.now()]], schema=["id", "name", "created_at"])
     
     # 新しい行を既存のDataFrameに追加
     df = df.union(new_row)
