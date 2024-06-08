d9d1aaabc07de19d155a26339639a9b72342964e
ryohei_1540 <ryohei_1540@hotmail.co.jp>
2024-06-08 15:41:36 +0000

    hoge

--- sql/tasks/simple_task.sql
+++ sql/tasks/simple_task.sql
@@ -2,4 +2,4 @@ CREATE OR REPLACE TASK simple_task
     WAREHOUSE = 'xsmall'
     SCHEDULE = 'USING CRON 0 * * * * UTC'
 AS
-    CALL simple_procedure('This is from a task');
+    CALL simple_procedure();
